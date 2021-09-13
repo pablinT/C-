1d1a8fe987be6f17b31b970a16c5f67135884559
Pablo Calderón <49917052+pablinT@users.noreply.github.com>
2021-09-13 17:33:29 -0300

    Add files via upload

---
+++ la_raiz_de.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{ 
+    
+    double resultado=sqrt(6.6);
+
+    cout<< resultado << endl;
+
+    system("pause");
+
+
+}
---
+++ math.operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;        // int data type for simple math
+    a = 9;
+    c = a + 1 + 1*0;
+    b = c++;
+ 
+    //cout << "los nros son " << a <<endl;
+    return 0;           // return main int as Exit Status 0 (success)
+} 
+
+// a=
